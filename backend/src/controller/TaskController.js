8108c15eb0bc6544911306f86d5c6d31c79e6ad4
Luan <luancpereira30@gmail.com>
2021-12-21 15:30:36 -0300

    Backend complete

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,15 @@
 const TaskModel = require("../model/TaskModel");
+const current = new Date();
+const {
+  startOfDay,
+  endOfDay,
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth,
+  startOfYear,
+  endOfYear,
+} = require("date-fns");
 
 class TaskController {
   async create(req, res) {
@@ -41,6 +52,114 @@ class TaskController {
       })
       .catch((error) => {
         return res.status(500).json(error);
+      });
+  }
+
+  async show(req, res) {
+    await TaskModel.findById(req.params.id)
+      .then((response) => {
+        if (response) {
+          return res.status(200).json(response);
+        }
+      })
+      .catch((error) => {
+        return res.status(404).json({ error: "Tarefa Não Encontrada!" });
+      });
+  }
+
+  async delete(req, res) {
+    await TaskModel.deleteOne({
+      _id: req.params.id,
+    })
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
+
+  async done(req, res) {
+    await TaskModel.findByIdAndUpdate(
+      { _id: req.params.id },
+      { done: req.params.done },
+      { new: true }
+    )
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
+
+  async late(req, res) {
+    await TaskModel.find({
+      when: { $lt: current },
+      macaddress: { $in: req.body.macaddress },
+    })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res.status(500).json(error);
+      });
+  }
+
+  async today(req, res) {
+    await TaskModel.find({
+      macaddress: { $in: req.body.macaddress },
+      when: { $gte: startOfDay(current), $lte: endOfDay(current) },
+    })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  }
+
+  async week(req, res) {
+    await TaskModel.find({
+      macaddress: { $in: req.body.macaddress },
+      when: { $gte: startOfWeek(current), $lte: endOfWeek(current) },
+    })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  }
+
+  async month(req, res) {
+    await TaskModel.find({
+      macaddress: { $in: req.body.macaddress },
+      when: { $gte: startOfMonth(current), $lte: endOfMonth(current) },
+    })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  }
+
+  async year(req, res) {
+    await TaskModel.find({
+      macaddress: { $in: req.body.macaddress },
+      when: { $gte: startOfYear(current), $lte: endOfYear(current) },
+    })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
       });
   }
 }
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -8,5 +8,12 @@ const MacaddressValidation = require("../middlewares/MacaddressValidation");
 router.post("/", TaskValidation, TaskController.create);
 router.put("/:id", TaskController.update);
 router.get("/filter/all", MacaddressValidation, TaskController.all);
-
+router.get("/:id", TaskController.show);
+router.delete("/:id", TaskController.delete);
+router.put("/:id/:done", TaskController.done);
+router.get("/filter/late", MacaddressValidation, TaskController.late);
+router.get("/filter/today", MacaddressValidation, TaskController.today);
+router.get("/filter/week", MacaddressValidation, TaskController.week);
+router.get("/filter/month", MacaddressValidation, TaskController.month);
+router.get("/filter/year", MacaddressValidation, TaskController.year);
 module.exports = router;
