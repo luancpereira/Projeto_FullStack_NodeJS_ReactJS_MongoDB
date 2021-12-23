1823fefbd6912dce06ee879b212f54ffbcc790d5
Luan <luancpereira30@gmail.com>
2021-12-23 11:13:59 -0300

    Continuação

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+function Footer() {
+  return (
+    <S.Container>
+        <span>ToDo - Organizando Sua Vida</span>
+    </S.Container>
+  );
+}
+
+export default Footer;
